ac9a2a03d43e76f6de89ddad1bd9a15163ac26e8
NoharaMasato <masato338914914@gmail.com>
2020-04-05 15:26:42 +0900

    bit全探索を追加

---
+++ libs/BinaryAllSearch.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+int main(){
+  int N = 4;
+// start
+  /*  0000
+      1000
+      0100
+      1100
+      0010 */
+  for(int bit(0);bit<(1<<N);bit++){
+    for(int i(0);i<N;i++){
+      if (((bit>>i) & 1)){
+        cout << 1;
+      }else{
+        cout << 0;
+      }
+    }
+    cout << endl;
+  }
+// end
+  return 0;
+}
