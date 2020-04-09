e653ae102b9f107f00042552e0c86a7bbdd6271c
NoharaMasato <masato338914914@gmail.com>
2020-04-10 08:55:53 +0900

    add gcd,lcm

--- master.cpp
+++ master.cpp
@@ -6,8 +6,6 @@ const int INFint = 2e9+1;
 const ll INFll = 2e18+1;
 ll MOD=1e9+7;
 
-ll gcd(ll a, ll b) { return __gcd(a,b); } //最大公約数
-ll lcm(ll a, ll b) { return a/gcd(a,b)*b; } //最大公倍数
 
 int main(){
   return 0;
