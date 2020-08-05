ce3cb578213025919fef334ae963eafe78f3bfc4
NoharaMasato <masato338914914@gmail.com>
2020-08-05 21:38:56 +0900

    add go master

---
+++ master.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+func abs(a int) int {
+    return int(math.Abs(float64(a)))
+}
+
+func pow(p, q int) int {
+    return int(math.Pow(float64(p), float64(q)))
+}
+
+func min(nums ...int) int {
+    if len(nums) == 0 {
+        panic("funciton min() requires at least one argument.")
+    }
+    res := nums[0]
+    for i := 0; i < len(nums); i++ {
+        res = int(math.Min(float64(res), float64(nums[i])))
+    }
+    return res
+}
+
+func max(nums ...int) int {
+    if len(nums) == 0 {
+        panic("funciton max() requires at least one argument.")
+    }
+    res := nums[0]
+    for i := 0; i < len(nums); i++ {
+        res = int(math.Max(float64(res), float64(nums[i])))
+    }
+    return res
+}
+
+func main() {
+  // 1 input
+  // var n int
+  // fmt.Scan(&n)
+
+  // multiinput
+  // var x, n int
+  // fmt.Scan(&x, &n)
+
+  // n input
+  // var t []int
+  // t := make([]int, n)
+  // for i := 0; i < n; i++ {
+  //   fmt.Scan(&t[i])
+  // }
+
+  // for statement
+  // sum := 0
+	// for i := 0; i < 10; i++ {
+	// 	sum += i
+	// }
+
+
+  // if statement
+  // x := 100
+	// if x == 50 {
+	// 	fmt.Println("Germany")
+	// } else if x == 100 {
+	// 	fmt.Println("Japan")
+	// } else {
+	// 	fmt.Println("Canada")
+	// }
+
+}
