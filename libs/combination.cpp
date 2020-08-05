ddea916b345203a0eb38b07271cc094239aab89e
NoharaMasato <masato338914914@gmail.com>
2020-08-05 21:39:12 +0900

    add combination

---
+++ libs/combination.cpp
@@ -0,0 +1,21 @@
+// https://algo-logic.info/combination/
+#include <bits/stdc++.h>
+
+using ll = long long;
+using namespace std;
+const int INFint = 1e9+1;
+const ll INFll = (ll)1e18+1;
+ll MOD=1e9+7;
+
+ll combination(int n,int k){// nCk
+  if(n<=1e7){ // 逆元を使ったフェルマー
+
+  }else if(n>1e7 && k <= 1e7){
+
+  }
+}
+
+int main(){
+  return 0;
+}
+
