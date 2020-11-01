8a2c8f460a00dc83c9114ca9cb7c059e091ad2bc
NoharaMasato <masato338914914@gmail.com>
2020-11-01 22:47:53 +0900

    add printa

--- master.cpp
+++ master.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
 
-using ll = long long;
 using namespace std;
+using ll = long long;
 const int INFint = 1e9+1;
 const ll INFll = (ll)1e18+1;
 ll MOD=1e9+7;
+#define printa(x,n) for(int i = 0; i < n; i++){ cout << (x[i]) << " \n"[i==n-1];};
 
 
 int main(){
