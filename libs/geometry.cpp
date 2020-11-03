9a85f2f5cc86effd394cd2c5027c30e1e95865dc
NoharaMasato <masato338914914@gmail.com>
2020-11-03 09:55:34 +0900

    add geometry

--- libs/geometry.cpp
+++ libs/geometry.cpp
@@ -25,6 +25,9 @@ public:
   P operator * (T d) {
     return P(x*d, y*d);
   }
+  bool operator == (P p) {
+    return (x == p.x && y == p.y);
+  }
   T dot(P p) { //内積
     return x*p.x + y*p.y;
   }
@@ -46,6 +49,27 @@ bool on_one_line(P<int> p1, P<int> p2, P<int> p3){
 // 直線p1, p2と直線q1, q2の交点
 P<int> intersection(P<int> p1, P<int> p2, P<int> q1, P<int> q2){
   return p1 + (p2 - p1)*((q2 - q1).det(q1 - p1) / (q2 - q1).det(p2 - p1));
+}
+
+// p1とp2の距離
+double point_to_point_distance(P<int> p1, P<int> p2) {
+  return sqrt((p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y));
+}
+
+// 点p1,p2を通る直線と点pの距離
+double point_to_line_distance(P<int> p1, P<int> p2, P<int> p){ // point to line distance
+  if(p1 == p2) return 0; //線分が点となる場合は例外
+
+  // https://mathtrain.jp/tentotyokusen （点と直線の公式参照)
+  int a = p2.y - p1.y, b = p1.x - p2.x, c = p2.x*p1.y - p2.y*p1.x;
+  double dist = abs(a*p.x + b*p.y + c)/sqrt(a*a+b*b);
+  return dist;
+}
+
+// https://www.geeksforgeeks.org/minimum-distance-from-a-point-to-the-line-segment-using-vectors/
+// 点p1,p2を結ぶ線分と点pの最短距離
+double point_to_line_segment_distance(P<int> p1, P<int> p2, P<int> p){
+
 }
 
 int main(){
