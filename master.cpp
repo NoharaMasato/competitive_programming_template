3b1b49c72226fab8a5d4ecff687ecb2771951cb5
NoharaMasato <masato338914914@gmail.com>
2021-01-10 09:35:35 +0900

    change mod

--- master.cpp
+++ master.cpp
@@ -4,7 +4,7 @@ using namespace std;
 using ll = long long;
 const int INFint = 1e9+1;
 const ll INFll = (ll)1e18+1;
-ll MOD=1e9+7;
+const int MOD=1e9+7;
 #define printa(x,n) for(int i = 0; i < n; i++){ cout << (x[i]) << " \n"[i==n-1];};
 
 
