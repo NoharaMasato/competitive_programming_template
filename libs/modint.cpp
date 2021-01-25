6b401d55daed3623e7e46f5d76eec82546f820e7
NoharaMasato <masato338914914@gmail.com>
2021-01-25 19:46:10 +0900

    change modint

--- libs/modint.cpp
+++ libs/modint.cpp
@@ -1,19 +1,20 @@
-// https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a#8-modint
+// https://qiita.com/drken/items/3b4fdf0a78e7a138cd9a#8-modint けんちょんさんのqiita
+// https://noshi91.hatenablog.com/entry/2019/03/31/174006 noshiさんのハテナ
 #include <bits/stdc++.h>
+const int MOD=1e9+7;
 using namespace std;
 
+// constexprを使っているのでMODを動的に決めることはできない(modを同的に決めたい時はnoshiさんのブログ↑を参考にする)
 // start
-// constexprを使っているのでMODを動的に決めることはできない
 template<int MOD> struct Fp {
   long long val;
   constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
     if (val < 0) val += MOD;
   }
-  constexpr int getmod() { return MOD; }
-  constexpr Fp operator - () const noexcept {
+  constexpr Fp operator - () const noexcept { // -bのように-1をかけるときに呼び出される
     return val ? MOD - val : 0;
   }
-  constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+  constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; } // 引数にint型を入れてもcastされてFp型になる
   constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
   constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
   constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
@@ -31,7 +32,7 @@ template<int MOD> struct Fp {
     val = val * r.val % MOD;
     return *this;
   }
-  constexpr Fp& operator /= (const Fp& r) noexcept {
+  constexpr Fp& operator /= (const Fp& r) noexcept { // rの逆元を拡張Euclidの控除法により求めて、valと掛け算を行っている
     long long a = r.val, b = MOD, u = 1, v = 0;
     while (b) {
         long long t = a / b;
@@ -51,8 +52,12 @@ template<int MOD> struct Fp {
   friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
     return os << x.val;
   }
-  // friend関数なので、引数を入れると勝手に呼ばれる
-  friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept { // aのn乗(オーダーlog(n)で計算できる)
+  friend constexpr istream& operator >> (istream &is, Fp<MOD>& x) noexcept {
+    is >> x.val;
+    x.val %= MOD;
+    return is;
+  }
+  friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept { //aのn乗O(log(n)), friend関数なので、引数を入れると勝手に呼ばれる
     if (n == 0) return 1;
     auto t = modpow(a, n / 2);
     t = t * t;
@@ -68,6 +73,8 @@ int main() {
   mint b = 74324;
   mint c = 13231;
   mint d = 8432455;
-
+  mint e;
+  cin>>e;
+  cout << e << endl;
   cout << (a * b + c) / d << endl;
 }
