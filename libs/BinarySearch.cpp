15b784f1df9beda93f6ae142d2c1bcec430e601c
NoharaMasato <masato338914914@gmail.com>
2021-03-17 20:34:03 +0900

    edit binary search

--- libs/BinarySearch.cpp
+++ libs/BinarySearch.cpp
@@ -1,25 +1,39 @@
-// https://atcoder.jp/contests/abc146/tasks/abc146_c
 #include <bits/stdc++.h>
 using ll = long long;
 using namespace std;
 
-int main(){
-  ll A,B,X;
-  cin>>A>>B>>X;
-// start
-  // ある条件を満たすギリギリの最大値を求める(大きいと条件を満たす場合は,okの条件が逆でrを出力する)
-  ll l = 0,r = 1e9+1;
-  while(l+1<r){
-    ll x = (l+r)/2;
-    bool ok = [&]{
-      ll nedan = A*x + B*int(to_string(x).size());
-      if (nedan <= X) return true;
-      else return false;
-    }();
-    if (ok) l = x;
+// 照準にソートされたAとそのサイズ、ターゲットの数字xを渡すとtarget以上の数字でもっともindexが小さいものを返す
+int my_lower_bound(vector<int> A, int size, int target){
+  int l = 0, r = size;
+  while (r - l > 0){
+    int x = (r + l) / 2;
+    if (A[x] < target) l = x + 1;
     else r = x;
   }
-  cout << l << endl;
-// end
+  return l;
+}
+
+int my_upper_bound(vector<int> A, int size, int target){
+  int l = 0, r = size;
+  while (r - l > 0){
+    int x = (r + l) / 2;
+    if (A[x] <= target) l = x + 1;
+    else r = x;
+  }
+  return l;
+}
+
+int main(){
+  vector<int> a = {1, 2, 2, 3, 5, 8, 10};
+  cout << my_lower_bound(a, 7, 0) << endl; // 0
+  cout << my_lower_bound(a, 7, 2) << endl; // 2以上で最初のindexなので1
+  cout << my_lower_bound(a, 7, 5) << endl; // 5以上なので4
+  cout << my_lower_bound(a, 7, 11) << endl; // 11以上はないので配列より外である7
+
+  cout << my_upper_bound(a, 7, 0) << endl; // 0
+  cout << my_upper_bound(a, 7, 2) << endl; // 2より大きい最初のindexなので3
+  cout << my_upper_bound(a, 7, 5) << endl; // 5
+  cout << my_upper_bound(a, 7, 11) << endl; // 11以上はないので配列より外である7
+
   return 0;
 }
--- typical_problems/StringDP.cpp
+++ typical_problems/StringDP.cpp
@@ -13,17 +13,15 @@ ll MOD=1e9+7;
 // N*Mの文字列に対して、common suffixを計算している(それの最大値が答え)
 int LongestCommonSubstring(string s, string t){
   int N = int(s.size()), M = int(t.size());
+  int ret(0);
   vector<vector<int>> dp(N+1, vector<int>(M+1, 0));
   for(int i(1);i<=N;i++){
     for(int j(1);j<=M;j++){
       if (s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
       else dp[i][j] = 0;
+      ret = max(ret, dp[i][j]);
     }
   }
-  int ret(0);
-  for(int i(1);i<=N;i++)
-    for(int j(1);j<=M;j++)
-      ret = max(ret, dp[i][j]);
   return ret;
 }
 
