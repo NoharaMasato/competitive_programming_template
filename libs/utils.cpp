1f487e63b2a29239276793e7a7a7c56567030b0c
NoharaMasato <masato338914914@gmail.com>
2021-05-30 09:38:45 +0900

    nearestを修正

--- libs/utils.cpp
+++ libs/utils.cpp
@@ -2,13 +2,13 @@
 using ll = long long;
 using namespace std;
 
-// abs(A[i]-x) [i:0~|A|-1]が最小のもののindexを返す（つまりはAの要素の中で、xに最も近い値のindexを返す)
+// abs(A[i]-x)が最小のもののindexを返す（つまりはAの要素の中で、xに最も近い値のindexを返す。返り値は0から|A|-1の範囲)
 // (注) Aは昇順にソート済みであるべき
-int nearest(vector<int> A, int x){
+int nearest(const vector<int> &A, int x){
   int N = int(A.size());
   int i = upper_bound(A.begin(), A.end(), x) - A.begin();
   if(i == N) return N-1;
-  else if(x == 0) return 0;
+  else if(i == 0) return 0;
   else{
     if(abs(A[i]-x) <= abs(A[i-1]-x)) return i;
     else return i-1;
