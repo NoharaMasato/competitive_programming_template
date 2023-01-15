d5c4ec4eb6c27db372247cb79b45ad193b8edce6
NoharaMasato <masato338914914@gmail.com>
2023-01-15 11:54:21 +0900

    Fmt master.go

--- master.go
+++ master.go
@@ -1,61 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"math"
+)
+
 func abs(a int) int {
-    return int(math.Abs(float64(a)))
+	return int(math.Abs(float64(a)))
 }
 
 func pow(p, q int) int {
-    return int(math.Pow(float64(p), float64(q)))
+	return int(math.Pow(float64(p), float64(q)))
 }
 
 func min(nums ...int) int {
-    if len(nums) == 0 {
-        panic("funciton min() requires at least one argument.")
-    }
-    res := nums[0]
-    for i := 0; i < len(nums); i++ {
-        res = int(math.Min(float64(res), float64(nums[i])))
-    }
-    return res
+	if len(nums) == 0 {
+		panic("function min() requires at least one argument.")
+	}
+	res := nums[0]
+	for i := 0; i < len(nums); i++ {
+		res = int(math.Min(float64(res), float64(nums[i])))
+	}
+	return res
 }
 
 func max(nums ...int) int {
-    if len(nums) == 0 {
-        panic("funciton max() requires at least one argument.")
-    }
-    res := nums[0]
-    for i := 0; i < len(nums); i++ {
-        res = int(math.Max(float64(res), float64(nums[i])))
-    }
-    return res
+	if len(nums) == 0 {
+		panic("function max() requires at least one argument.")
+	}
+	res := nums[0]
+	for i := 0; i < len(nums); i++ {
+		res = int(math.Max(float64(res), float64(nums[i])))
+	}
+	return res
 }
 
 func main() {
-  // 1 input
-  // var n int
-  // fmt.Scan(&n)
+	// multi input
+	// var x, n int
+	// fmt.Scan(&x, &n)
 
-  // multiinput
-  // var x, n int
-  // fmt.Scan(&x, &n)
+	// n input
+	// var n int
+	// fmt.Scan(&n)
+	// t := make([]int, n)
+	// for i := 0; i < n; i++ {
+	// 	fmt.Scan(&t[i])
+	// }
 
-  // n input
-  // var t []int
-  // t := make([]int, n)
-  // for i := 0; i < n; i++ {
-  //   fmt.Scan(&t[i])
-  // }
-
-  // for statement
-  // sum := 0
+	// for statement
+	// sum := 0
 	// for i := 0; i < 10; i++ {
 	// 	sum += i
 	// }
 
-
-  // if statement
-  // x := 100
+	// if statement
+	// x := 100
 	// if x == 50 {
 	// 	fmt.Println("Germany")
 	// } else if x == 100 {
